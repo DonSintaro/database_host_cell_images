f4e38a3ca4e0bf08890649500db9441608522b48
DonSintaro <jarriaga.important@gmail.com>
2020-04-30 17:46:17 -0500

    conveted to axios

--- server.js
+++ server.js
@@ -17,18 +17,32 @@ app.get("/api/awaken", function(req, res) {
     res.send("awake");
 });
 
+// app.post("/api/create/:name", function(req, res) {
+//     let buffer = req.params.name;
+//     console.log(buffer);
 
-app.get("/api/create/:name", function(req, res) {
-    let buffer = req.params.name;
-    console.log(buffer);
+//     let cellLink = "gs://cell-image-project.appspot.com/Cells/Neutrophils/" + buffer +".jpg, neutrophil";
+
+//     db.create(cellLink).then(function(){
+//         res.send("Success");
+//     });
+// });
+
+app.post("/api/create/:name", async function({body}, res) {
     
-    let cellLink = "gs://cell-image-project.appspot.com/Cells/Neutrophils/" + buffer +".jpg, neutrophil";
+    let cellLink = "gs://cell-image-project.appspot.com/Cells/Neutrophils/" + body +".jpg, neutrophil";
 
     db.create(cellLink).then(function(){
         res.send("Success");
     });
 });
 
+
+app.post("/api/create/:id", async function({params, body}, res){
+    
+    let data = await books.deleteBook(params,body)
+    res.send(data);
+});
 
 app.listen(PORT, function() {
     console.log("App now listening at localhost:" + PORT);
