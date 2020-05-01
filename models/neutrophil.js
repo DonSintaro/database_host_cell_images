afdc6dfd7ab2c07e3bfc60635ffd9fd3b86129af
DonSintaro <jarriaga.important@gmail.com>
2020-04-30 19:48:34 -0500

    testing formats

--- models/neutrophil.js
+++ models/neutrophil.js
@@ -1,6 +1,10 @@
 module.exports = function(sequelize,DataTypes){
     const Model = sequelize.define('neutrophil', {
-    
+        id: {type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+            },
+
         link: {type: DataTypes.STRING, 
             allowNull: false
             }
