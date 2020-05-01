bb22528deecac910baf2cbd3df01f7aa8fc9a8b4
DonSintaro <jarriaga.important@gmail.com>
2020-04-30 19:44:15 -0500

    correction

--- models/neutrophil.js
+++ models/neutrophil.js
@@ -1,10 +1,6 @@
 module.exports = function(sequelize,DataTypes){
     const Model = sequelize.define('neutrophil', {
-        id: {type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-            },
-
+    
         link: {type: DataTypes.STRING, 
             allowNull: false
             }
